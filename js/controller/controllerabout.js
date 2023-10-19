75a8966dd27a4af59f2eb1328227430fb9006ffa
Jxxzyshn <71040617+jxxzyshncyberoren@users.noreply.github.com>
2023-10-19 13:07:52 +0700

    Delete js directory
    .

--- js/controller/controllerabout.js
+++
@@ -1,5 +0,0 @@
-import { get } from "https://jscroot.github.io/api/croot.js";
-import { responseLatarbelakang} from "../config/config.js";
-import { JSONlink } from "../template/template.js";
-
-get(JSONlink, responseLatarbelakang);
--- js/controller/controllerteams.js
+++
@@ -1,7 +0,0 @@
-import { get } from "https://jscroot.github.io/api/croot.js";
-import { ProfilePicture, ResponseJoss, profilejoss, responseTeam} from "../config/config.js";
-import { JSONTeams, JSONlink, JSONjoss } from "../template/template.js";
-
-get(JSONTeams, responseTeam);
-get(JSONjoss, ResponseJoss);
-get(JSONlink, ProfilePicture)
